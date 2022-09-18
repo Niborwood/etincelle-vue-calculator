40fe622b3256637f33a9918002a9fde70571f458
Robin SOURIAU <robin.souriau@gmail.com>
2022-09-18 21:15:12 +0200

    Better UI and data for classes

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,20 @@ const handleSubmit = () => {
 };
 </script>
 
-<template class="bg-slate-900">
+<template>
   <div
-    class="flex flex-col items-center justify-center min-h-screen gap-4 min-w-screen font-quicksand text-stone-700"
+    class="flex flex-col items-center justify-center min-h-screen gap-4 p-4 min-w-screen font-quicksand text-stone-700"
   >
     <header>
       <h1 class="text-5xl font-bold">Calculateur Etincelle</h1>
     </header>
-    <main class="w-2/3 p-8">
+    <main class="md:p-8 md:w-2/3">
       <form
         @submit.prevent="handleSubmit"
         class="flex flex-col items-start justify-start gap-4"
       >
         <InputGroup />
-        <button type="submit">Envoyer</button>
+        <button type="submit">Suivant</button>
       </form>
     </main>
   </div>
--- src/components/InputGroup.vue
+++ src/components/InputGroup.vue
@@ -1,35 +1,69 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { Circle, CheckCircle } from "lucide-vue-next";
 
 const lessonItems = ref([
   {
     id: "enfants-1",
     label: "Enfants 1",
-    description: "Pour les startups",
+    details: "+ 4 ans",
+    day: "Samedi",
+    hours: "13h00 - 13h45",
+    room: "Salle 4",
     price: 220,
   },
   {
     id: "enfants-2",
     label: "Enfants 2",
-    description: "Pour les entreprises en croissance",
+    details: "+ 8 ans",
+    day: "Samedi",
+    hours: "10h15 - 11h30",
+    room: "Salle 4",
     price: 220,
   },
   {
     id: "concours-13",
     label: "Concours - de 13 ans",
-    description: "Pour les entreprises à l'échelle",
+    details: "+ 9 ans",
+    day: "Samedi",
+    hours: "11h00 - 13h00",
+    room: "Salle 4",
     price: 250,
   },
   {
     id: "ados-1",
     label: "Ados 1",
-    description: "Pour les entreprises à l'échelle",
+    details: "+ 12 ans",
+    day: "Vendredi",
+    hours: "18h30 - 19h45",
+    room: "Salle 5",
     price: 270,
   },
   {
     id: "avances",
     label: "Avancés",
-    description: "Pour les entreprises à l'échelle",
+    details: "Bon niveau technique exigé",
+    day: "Mardi",
+    hours: "20h00 - 21h30",
+    room: "Salle 5",
+    price: 270,
+  },
+  {
+    id: "concours-13-18",
+    label: "Concours 13 - 18 ans",
+    details: "De 13 à 18 ans",
+    day: "Vendredi",
+    hours: "19h45 - 21h45",
+    room: "Salle 5",
+    price: 270,
+  },
+  {
+    id: "atelier-chore",
+    label: "Atelier chorégraphique",
+    details: "Solo, duo, trio",
+    hours: "13h45 - 15h15",
+    day: "Samedi",
+    room: "Salle 3",
     price: 270,
   },
 ]);
@@ -37,13 +71,18 @@ const checkedLessons = ref<string[]>([]);
 </script>
 
 <template>
-  <div class="text-2xl text-bold">Cours choisis</div>
-  <div class="flex flex-row flex-wrap items-center gap-4">
+  <div class="text-lg text-bold">Cours choisis</div>
+  <div
+    class="flex flex-col flex-1 gap-4 md:items-center md:flex-row md:flex-wrap"
+  >
     <div
       v-for="input in lessonItems"
       :key="input.id"
-      class="border-2 rounded-lg cursor-pointer"
-      :class="checkedLessons.includes(input.id) && 'border-orange-300'"
+      class="flex flex-row-reverse p-2 border-2 rounded-lg cursor-pointer hover:shadow-md md:basis-64 md:grow-0"
+      :class="
+        checkedLessons.includes(input.id) &&
+        'border-orange-300 bg-gradient-to-tr from-orange-200/90 to-orange-200'
+      "
     >
       <input
         type="checkbox"
@@ -52,9 +91,23 @@ const checkedLessons = ref<string[]>([]);
         :value="input.id"
         v-model="checkedLessons"
       />
-      <label class="block p-8 cursor-pointer select-none" :for="input.id">{{
-        input.label
-      }}</label>
+
+      <div v-if="checkedLessons.includes(input.id)">
+        <CheckCircle class="text-orange-500" />
+      </div>
+      <div v-else><Circle class="text-stone-900/50" /></div>
+
+      <label
+        class="flex-1 block p-4 space-y-2 transition-all cursor-pointer select-none"
+        :for="input.id"
+      >
+        <div class="flex-1 font-bold">{{ input.label }}</div>
+        <div>
+          <div class="text-sm">{{ input.details }}</div>
+          <div class="text-sm">{{ input.day }}, {{ input.hours }}</div>
+          <div class="text-sm text-stone-900/60">{{ input.room }}</div>
+        </div>
+      </label>
     </div>
   </div>
 </template>
