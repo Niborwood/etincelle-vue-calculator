d31c16e53b362d04e90c4b7b5e53112efa4c37ca
Robin SOURIAU <robin.souriau@gmail.com>
2022-10-22 11:23:41 +0200

    Title is left aligned

--- src/components/ui/title-form.vue
+++ src/components/ui/title-form.vue
@@ -1,7 +1,5 @@
 <template>
-  <h2
-    class="px-6 pb-8 mb-8 text-4xl font-semibold text-center border-b-2 border-stone-700"
-  >
+  <h2 class="px-6 pb-8 mb-8 text-3xl font-semibold border-b-2 border-stone-700">
     <slot />
   </h2>
 </template>
