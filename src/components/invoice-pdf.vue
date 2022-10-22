245ccb1f6ecd4fbe03e958b21f90420eca96a5ff
Robin Souriau <robin.souriau@gmail.com>
2022-10-22 16:18:25 +0200

    Install html2pdf, base for pdf

---
+++ src/components/invoice-pdf.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import VueHtml2pdf from "html2pdf.js";
+
+const exportToPDF = () => {
+  const element = document.getElementById("invoice");
+  const options = {
+    margin: 0,
+    filename: "invoice.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+  };
+  VueHtml2pdf().from(element).set(options).save();
+};
+</script>
+
+<template>
+  <div class="space-y-8">
+    <button @click="exportToPDF">Export to PDF</button>
+    <div id="invoice" class="bg-stone-600">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+      adipisci nisi maxime, harum dolore pariatur ullam. Doloribus sequi, ad
+      deleniti incidunt maiores quaerat mollitia distinctio dolorem sapiente
+      nulla culpa eum inventore enim nemo quibusdam repellendus! Nulla
+      asperiores natus odio nam eum iste corporis fuga eos eius accusantium
+      aperiam voluptates quas sint reprehenderit, porro fugiat, eaque, laborum
+      excepturi! Facere molestiae ad expedita ex et tempore quos eaque aliquid.
+      Quas molestiae illo provident laborum aliquid voluptas! Odio eius labore
+      porro! Optio et ullam magni earum assumenda odio culpa dignissimos
+      excepturi sunt, placeat ab reprehenderit ea rerum repellendus cum esse. At
+      eos quia neque quod asperiores voluptates, deleniti porro cum odio eum
+      architecto dicta unde amet tempore in sequi reiciendis similique,
+      laboriosam sit et nihil deserunt nobis fugiat. Laboriosam, error.
+      Temporibus.
+    </div>
+  </div>
+</template>
--- src/components/sections/invoice-section.vue
+++ src/components/sections/invoice-section.vue
@@ -1,6 +1,22 @@
 <script setup lang="ts">
 import { TitleForm } from "@/components/ui";
-import { Download, Mail } from "lucide-vue-next";
+import { Download } from "lucide-vue-next";
+import VueHtml2pdf from "html2pdf.js";
+import { useAppStore } from "@/stores/AppStore";
+
+const store = useAppStore();
+
+const exportToPDF = () => {
+  const element = document.getElementById("invoice");
+  const options = {
+    margin: 0,
+    filename: "invoice.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+  };
+  VueHtml2pdf().from(element).set(options).save();
+};
 </script>
 
 <template>
@@ -24,8 +40,85 @@ import { Download, Mail } from "lucide-vue-next";
     <button
       type="button"
       class="flex items-center gap-2 p-4 text-xl font-semibold tracking-wider transition-shadow bg-orange-400 rounded-lg hover:shadow-md text-slate-100 hover:bg-orange-500"
+      @click="exportToPDF"
     >
       <Download /> Télécharger le bulletin de&nbsp;pré-inscription
     </button>
+
+    <div class="p-2 border">
+      <div id="invoice" class="p-2">
+        <div class="flex gap-2 mb-8 text-7xl">Ecole de danse Etincelle</div>
+
+        <!-- Informations -->
+        <div class="flex items-center gap-2 mb-2">
+          <p class="text-2xl text-orange-900 uppercase">
+            Bulletin de pré-inscription web
+          </p>
+          <p class="italic text-stone-400">(à remplir en majuscules)</p>
+        </div>
+
+        <div class="">
+          <div class="text-slate-600">
+            <span class="text-lg text-stone-900">Nom :</span>
+            {{ store.formValues.surname }}
+          </div>
+          <div class="text-slate-600">
+            <span class="text-lg text-stone-900">Prénom :</span>
+            {{ store.formValues.name }}
+          </div>
+          <div class="text-slate-600">
+            <span class="text-lg text-stone-900">Date de naissance :</span>
+            {{ new Date(store.formValues.birthday).toLocaleDateString() }}
+          </div>
+          <div class="text-slate-600">
+            <span class="text-lg text-stone-900"
+              >Nom du responsable légal :</span
+            >
+            {{ store.formValues.resp_name }}
+          </div>
+          <div class="text-slate-600">
+            <span class="text-lg text-stone-900"
+              >Portable du responsable légal :</span
+            >
+            {{ store.formValues.resp_phone }}
+          </div>
+          <div class="text-slate-600">
+            <span class="text-lg text-stone-900">Portable de l'élève :</span>
+            {{ store.formValues.phone }}
+          </div>
+          <div class="text-slate-600">
+            <span class="text-lg text-stone-900"
+              >Mail du responsable légal :</span
+            >
+            {{ store.formValues.resp_email }}
+          </div>
+          <div class="text-slate-600">
+            <span class="text-lg text-stone-900">Adresse :</span>
+            {{ store.formValues.address }}
+          </div>
+          <div class="text-slate-600">
+            <span class="text-lg text-stone-900">Ville :</span>
+            {{ store.formValues.zipcode }}, {{ store.formValues.city }}
+          </div>
+        </div>
+
+        <!-- Cours choisis -->
+        <div class="my-4 space-y-1">
+          <!-- Titre -->
+          <div class="flex items-center gap-2 mb-2">
+            <p class="text-2xl italic font-bold text-orange-900">
+              Cours choisis
+            </p>
+            <p class="italic text-stone-400">
+              (cochez la ou les cases correspondantes)
+            </p>
+          </div>
+
+          <!-- Street & Lyrical -->
+
+          <!-- Lyrical & Modern Jazz -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
